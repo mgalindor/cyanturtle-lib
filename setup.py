985f913ec490e92273c64d1edb55aa8425203a68
Miguel Galindo <mgalindo@yellowpepper.com>
2020-05-24 15:41:01 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cyanturtle-lib',
+      version='0.1.1',
+      description='Util functions of flask-jwt,marshmallow and repository patterns',
+      url='https://github.com/mgalindor/cyanturtle-lib',
+      author='Miguel Galindo',
+      author_email='mgldrz@gmail.com',
+      license='MIT',
+      packages=['cyanturtle'],
+      zip_safe=False)
